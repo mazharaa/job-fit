bd50944bc3752fa6e9cc5964b1841cfaacb665e6
bismarko_nobilas <bismarko139@gmail.com>
2025-07-20 15:09:50 +0700

    feat:repair-login

--- src/app/(auth)/_components/social-login.js
+++ src/app/(auth)/_components/social-login.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import { Button } from "@/components/ui/button";
-
 import { googleLoginAction } from "../action";
 
 export default function SocialLogin() {
--- src/services/loguser.js
+++ src/services/loguser.js
@@ -1,15 +1,18 @@
 import "server-only";
-import { prisma } from "@/utils/prisma";
+import prisma from "@/utils/prisma";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export async function getLogUser() {
   const cookieStore = await cookies();
-  const isUserId = cookieStore.get("userId")?.value;
+  const isUserId = cookieStore.get("userId").value;
 
+  console.log("Check userid");
+  console.log(isUserId);
   if (!isUserId) {
     redirect("/");
   }
+  console.log("Check log user");
 
   const logUser = await prisma.user.findFirst({
     where: {
@@ -25,6 +28,8 @@ export async function getLogUser() {
   if (!logUser) {
     redirect("/");
   }
+
+  console.log(logUser);
 
   return logUser;
 }
